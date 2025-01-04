c60f66d41944f8c9ffa593a2a78636bd866d39c4
Alexey <pirogov30002@gmail.com>
2025-01-04 13:02:06 +0300

    add: prometheus requirement

--- api/config/settings.py
+++ api/config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "drf_spectacular",
+    'django_prometheus',
     "ids",
 ]
 
@@ -64,6 +65,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = "config.urls"
