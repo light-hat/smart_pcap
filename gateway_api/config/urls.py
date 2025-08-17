2862203df5adf28a631b434a6c8005acf7344513
Alexey <pirogov30002@gmail.com>
2025-08-17 17:00:41 +0300

    integrate front vs back in docker

--- gateway_api/config/urls.py
+++ gateway_api/config/urls.py
@@ -3,12 +3,30 @@
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views import View
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+
+
+class SPAView(View):
+    """
+    Отображение фронтэнда.
+    """
+
+    def get(self, request, *args, **kwargs):
+        """
+        GET запрос для бандла.
+        """
+        index_file = os.path.join(settings.STATIC_ROOT, "index.html")
+        if os.path.exists(index_file):
+            with open(index_file, "rb") as f:
+                return HttpResponse(f.read(), content_type="text/html")
+        raise Http404("index.html not found in STATIC_ROOT")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -20,4 +38,5 @@ urlpatterns = [
         name="swagger-ui",
     ),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    re_path(r"^(?!api|admin).*", SPAView.as_view(), name="spa"),
 ]
